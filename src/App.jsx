ea60762316888e50f0cc17abd36b1b09da877ea5
J Rose <josh@upros3.com>
2018-02-20 01:18:47 +0000

    finished first componants exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
+    console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+        <div>
+          <Message/>
+          <MessageList/>
+          <ChatBar/>
+        </div>
+
     );
   }
 }
