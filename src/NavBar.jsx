a9e2b08bfd38960a0b447ba0ad1ceaeca784068e
J Rose <josh@upros3.com>
2018-02-23 06:52:31 +0000

    fixed grammar for number of clients

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class NavBar extends Component {
   render() {
-    const { loggedInUsers } = this.props;
+    const loggedInUsers = this.props.loggedInUsers;
+    let grammar = "users";
+
+    if (this.props.loggedInUsers === 1) {
+      console.log("poop");
+      grammar = "user";
+    }
 
     return (
       <nav className="navbar">
@@ -10,7 +16,7 @@ class NavBar extends Component {
           ChattyApp
         </a>
         <span className="logged-in-users">
-          {loggedInUsers} users logged in!
+          {loggedInUsers} {grammar} logged in
         </span>
       </nav>
     );
